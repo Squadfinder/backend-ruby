518c9eab19b3d2309cfe468d98af013dcca00659
Blake Saylor <Blakesaylor@gmail.com>
2022-10-13 14:39:28 -0600

    Setup test table for Heroku

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'relationships' do
+
+  end
+
+  before :each do
+
+  end
+
+  describe 'class methods' do
+    
+  end
+
+  describe 'instance methods' do
+    
+  end
+end
