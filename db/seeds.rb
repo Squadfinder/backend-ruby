c2373c72d62052cdbf6103674f1c8306efbb36ad
Blake Saylor <Blakesaylor@gmail.com>
2022-10-13 17:16:24 -0600

    Fix welcome page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-test = User.new(name: 'test')
+test_user = User.create!(name: 'test')
