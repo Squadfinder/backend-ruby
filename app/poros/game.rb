e64a51877e40580eba59561ceefc8098e1e19070
Michael Koulouvaris <mikekoul@gmail.com>
2022-10-15 18:55:56 -0600

    test/Past test for game poros

--- app/poros/game.rb
+++ app/poros/game.rb
@@ -1,3 +1,11 @@
 class Game
-  
+  attr_reader :id, :name, :background_image, :genres, :parent_platforms
+
+  def initialize(data)
+    @id = data[:id]
+    @name = data[:name]
+    @background_image = data[:background_image]
+    @genres = data[:genres]
+    @parent_platforms = data[:parent_platforms]
+  end
 end
--- spec/poros/game_spec.rb
+++ spec/poros/game_spec.rb
@@ -24,6 +24,15 @@ RSpec.describe Game do
           },
         ]
       }
+
+      game = Game.new(data)
+
+      expect(game).to be_a(Game)
+      expect(game.id).to eq(4200)
+      expect(game.name).to eq("Portal 2")
+      expect(game.background_image).to eq("https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg")
+      expect(game.genres).to be_a(Array)
+      expect(game.parent_platforms).to be_a(Array)
     end
   end
 end 
