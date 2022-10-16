55c40e50e7c34bc955e8cfebba9ddee8c2839919
Michael Koulouvaris <mikekoul@gmail.com>
2022-10-15 19:00:14 -0600

    test/Passed rawg facade test

--- spec/facades/rawg_facade_spec.rb
+++ spec/facades/rawg_facade_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe RawgFacade do
 
     game = RawgFacade.get_game(4200)
 
-    expect(game).to be_a(Hash)
+    expect(game).to be_a(Game)
+    expect(game.id).to eq(4200)
+    expect(game.name).to eq("Portal 2")
+    expect(game.background_image).to eq("https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg")
+    expect(game.genres).to be_a(Array)
+    expect(game.parent_platforms).to be_a(Array)
   end
 end
