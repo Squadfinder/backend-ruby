c8b9d2e2a0f9b23f500dc5079646fa57847446bc
Michael Koulouvaris <mikekoul@gmail.com>
2022-10-15 20:20:56 -0600

    Test for successful response passed

---
+++ app/controllers/api/v1/games_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::GamesController < ApplicationController
+  before_action :param_validation
+
+  def show
+    game = RawgFacade.get_game(params[:game_id])
+    render json: game
+  end
+
+  private
+  def param_validation
+    if !params[:game_id].present?
+      render json: { data: {} }, status:400
+    end
+  end
+end
--- app/poros/game.rb
+++ app/poros/game.rb
@@ -1,11 +1,11 @@
 class Game
-  attr_reader :id, :name, :background_image, :genres, :parent_platforms
+  attr_reader :id, :title, :image, :genres, :consoles
 
   def initialize(data)
     @id = data[:id]
-    @name = data[:name]
-    @background_image = data[:background_image]
+    @title = data[:name]
+    @image = data[:background_image]
     @genres = data[:genres]
-    @parent_platforms = data[:parent_platforms]
+    @consoles = data[:parent_platforms]
   end
 end
