fa6533f82d7da9da48a608aa30d0f28a9abb0dfc
Michael Koulouvaris <mikekoul@gmail.com>
2022-10-16 12:19:57 -0600

    Refactor test

--- spec/facades/rawg_facade_spec.rb
+++ spec/facades/rawg_facade_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe RawgFacade do
 
     expect(game).to be_a(Game)
     expect(game.id).to eq(4200)
-    expect(game.name).to eq("Portal 2")
-    expect(game.background_image).to eq("https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg")
+    expect(game.title).to eq("Portal 2")
+    expect(game.image).to eq("https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg")
     expect(game.genres).to be_a(Array)
-    expect(game.parent_platforms).to be_a(Array)
+    expect(game.consoles).to be_a(Array)
   end
 end
