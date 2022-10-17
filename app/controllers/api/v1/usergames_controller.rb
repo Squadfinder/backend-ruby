dcbcb18060ad9c8c10febbbf5375435ca4b7eb47
Wesley Miranda <wesleywes42@gmail.com>
2022-10-17 17:14:49 -0600

    user games create has been created

--- app/controllers/api/v1/usergames_controller.rb
+++ app/controllers/api/v1/usergames_controller.rb
@@ -4,6 +4,19 @@ class Api::V1::UsergamesController < ApplicationController
     render json: UserGameSerializer.new(UserGame.list(params[:user_id]))
   end
 
+  def create
+    user = User.find_by_id(params[:user_id])
+    user_game = UserGame.new(user_params)
+    if user_game.save
+      render json: UserGameSerializer.new(user_game), status: 201
+    else
+      render json: { error: 'Email Or Password Is Incorrect' }, status: 401
+    end
+  end
 
 
+  private
+    def user_params
+      params.permit(:user_id, :game_id, :image_url, :game_title)
+    end
 end
