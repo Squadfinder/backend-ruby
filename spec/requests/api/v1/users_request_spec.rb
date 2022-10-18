7dcc83d8a2eb25745facc30e75af24f6ec5ebcf1
Blake Saylor <Blakesaylor@gmail.com>
2022-10-18 10:54:18 -0600

    Refactoring serializers and reformatting file structure.
    Co-authored-by: Thomas Turner <47160926+eagleeye5085@users.noreply.github.com>
    Co-authored-by: Mike Koulouvaris <44381885+mikekoul@users.noreply.github.com>
    Co-authored-by: Wesley Miranda <54329737+wes30303@users.noreply.github.com>

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe "Users API" do
   it 'Can return a list of all Users' do
     create_list(:user, 5)
+    usergame1 = UserGame.create!(user_id: User.first.id, game_id: 2343, image_url: "www.pic.com/image.img", game_title: "Halo")
+    usergame2 = UserGame.create!(user_id: User.second.id, game_id: 2387, image_url: "www.pic.com/image.img", game_title: "Squad")
+    usergame3 = UserGame.create!(user_id: User.third.id, game_id: 2323, image_url: "www.pic.com/image.img", game_title: "Scorn")
+    usergame4 = UserGame.create!(user_id: User.fourth.id, game_id: 2356, image_url: "www.pic.com/image.img", game_title: "Rocket Leauge")
+    usergame5 = UserGame.create!(user_id: User.fifth.id, game_id: 2343, image_url: "www.pic.com/image.img", game_title: "Halo")
+    usergame6 = UserGame.create!(user_id: User.first.id, game_id: 1234, image_url: "www.pic.com/image.img", game_title: "bing bong")
 
     get api_v1_users_path
 
@@ -23,11 +29,8 @@ describe "Users API" do
       expect(user).to have_key(:attributes)
       expect(user[:attributes]).to be_a Hash
 
-      # Update when we have gamesList in here
-      expect(user[:attributes].keys.count).to eq 2
-      expect(user[:attributes].keys).to include(:gamertag, :platform)
-      # expect(user[:attributes].keys.count).to eq 3
-      # expect(user[:attributes].keys).to include(:gamertag, :platform, :games_list)
+      expect(user[:attributes].keys.count).to eq 3
+      expect(user[:attributes].keys).to include(:gamertag, :platform, :user_games)
 
       expect(user[:attributes][:gamertag]).to be_a String
       expect(user[:attributes][:gamertag]).to eq User.find(user[:id]).gamertag
@@ -35,8 +38,23 @@ describe "Users API" do
       expect(user[:attributes][:platform]).to be_a String
       expect(user[:attributes][:platform]).to eq User.find(user[:id]).platform
 
-      # expect(user[:attributes][:games_list]).to be_a Array
-      # expect(user[:attributes][:games_list]).to eq User.find(user[:id]).games_list
+      expect(user[:attributes][:user_games]).to be_a Array
+
+      expect(user[:attributes][:user_games].count).to eq User.find_by_id(user[:id]).user_games.count
+
+      user[:attributes][:user_games].each do |user_game|
+        expect(user_game).to be_a Hash
+        expect(user_game.keys.count).to eq 7
+        expect(user_game.keys).to include(:id, :user_id, :game_id, :image_url, :game_title, :created_at, :updated_at)
+
+        expect(user_game[:id]).to be_a Integer
+        expect(user_game[:user_id]).to be_a Integer
+        expect(user_game[:game_id]).to be_a Integer
+        expect(user_game[:image_url]).to be_a String
+        expect(user_game[:game_title]).to be_a String
+        expect(user_game[:created_at]).to be_a String
+        expect(user_game[:updated_at]).to be_a String
+      end
     end
   end
 
@@ -50,7 +68,7 @@ describe "Users API" do
 
     get api_v1_user_path(user)
 
-    result = JSON.parse(response.body, symbolize_names: true)
+    result = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
 
     expect(response).to be_successful
 
@@ -58,14 +76,14 @@ describe "Users API" do
     expect(result[:gamertag]).to be_a(String)
     expect(result).to have_key(:platform)
     expect(result[:platform]).to be_a(String)
-    expect(result).to have_key(:games_list)
-    expect(result[:games_list]).to be_a(Array)
-    expect(result[:games_list].count).to eq(4)
-    expect(result[:games_list][0]).to have_key(:game_id)
-    expect(result[:games_list][0][:game_id]).to be_a(Integer)
-    expect(result[:games_list][0]).to have_key(:game_title)
-    expect(result[:games_list][0][:game_title]).to be_a(String)
-    expect(result[:games_list][0]).to have_key(:image_url)
-    expect(result[:games_list][0][:image_url]).to be_a(String)
+    expect(result).to have_key(:user_games)
+    expect(result[:user_games]).to be_a(Array)
+    expect(result[:user_games].count).to eq(4)
+    expect(result[:user_games][0]).to have_key(:game_id)
+    expect(result[:user_games][0][:game_id]).to be_a(Integer)
+    expect(result[:user_games][0]).to have_key(:game_title)
+    expect(result[:user_games][0][:game_title]).to be_a(String)
+    expect(result[:user_games][0]).to have_key(:image_url)
+    expect(result[:user_games][0][:image_url]).to be_a(String)
   end
 end
