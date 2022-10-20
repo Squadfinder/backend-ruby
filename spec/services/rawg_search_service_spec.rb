7e6d5daaf7a1ebe069137545582b0f0e9661ae7e
Wesley Miranda <wesleywes42@gmail.com>
2022-10-19 23:05:10 -0600

    services test

--- spec/services/rawg_search_service_spec.rb
+++ spec/services/rawg_search_service_spec.rb
@@ -13,5 +13,16 @@ describe RawgSearchService do
       expect(search[:results][0][:platforms]).to be_a Array
       expect(search[:results][0][:name]).to be_a String
     end
+    it "returns the information from the games with genres and attributes" do
+      search = RawgSearchService.get_name_and_genre('call of duty', 'shooter')
+      expect(search).to be_a Hash
+      expect(search[:results][0]).to have_key(:background_image)
+      expect(search[:results][0]).to have_key(:platforms)
+      expect(search[:results][0]).to have_key(:name)
+
+      expect(search[:results][0][:background_image]).to be_a String
+      expect(search[:results][0][:platforms]).to be_a Array
+      expect(search[:results][0][:name]).to be_a String
+    end
   end
 end
