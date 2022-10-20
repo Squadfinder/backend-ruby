c7f1e7a0f82684ded48c93958e50f6b7f26e361c
Michael Koulouvaris <mikekoul@gmail.com>
2022-10-19 18:13:52 -0600

    test/Refactor some squad and userSquad tests to reflect current functionality

--- spec/requests/api/v1/users/squads/squads_request_spec.rb
+++ spec/requests/api/v1/users/squads/squads_request_spec.rb
@@ -35,61 +35,6 @@ describe 'UserSquads API' do
     expect(result[0][:updated_at]).to be_a(String)
   end
 
-  it 'Can create a new UserSquad when given a host and a squad with the status as pending' do
-    host_user = create(:user)
-    user_2 = User.create!(gamertag: "IMbad", platform: "x-box")
-    user_3 = User.create!(gamertag: 'thomas', platform: 'XBox')
-    user_4 = User.create!(gamertag: 'wes', platform: 'XBox')
-
-    create_squad_body = {
-      'user': host_user.id,
-      'game':  Faker::Game.title,
-      'event_time': Faker::Time.forward(days: 5),
-      'number_players': 3,
-      'competitive': true,
-      'squadMembers': [
-      user_2,
-      user_3,
-      user_4
-    ]
-    }
-
-    post api_v1_squads_path(create_squad_body)
-
-    invited_user = create(:user)
-
-    input_body = {
-      'host_id': host_user.id,
-      'squad_id': Squad.last.id
-    }
-
-    post api_v1_user_squads_path(invited_user, input_body)
-
-    expect(response).to be_successful
-    expect(response.status).to eq 201
-
-    response_body = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response_body).to be_a Hash
-    expect(response_body.keys.count).to eq 7
-    expect(response_body.keys).to include(:id, :user_id, :squad_id, :host_id, :status, :created_at, :updated_at)
-
-    expect(response_body[:user_id]).to be_a Integer
-    expect(response_body[:user_id]).to eq invited_user.id
-
-    expect(response_body[:squad_id]).to be_a Integer
-    expect(response_body[:squad_id]).to eq Squad.last.id
-
-    expect(response_body[:host_id]).to be_a Integer
-    expect(response_body[:host_id]).to eq host_user.id
-
-    expect(response_body[:status]).to be_a String
-    expect(response_body[:status]).to eq 'pending'
-
-    expect(response_body[:created_at]).to be_a String
-    expect(response_body[:updated_at]).to be_a String
-  end
-
   it 'Can delete UserSquad association' do
     user_1 = User.create!(gamertag: "sorryIMbad", platform: "x-box")
     user_2 = User.create!(gamertag: "IMbad", platform: "x-box")
