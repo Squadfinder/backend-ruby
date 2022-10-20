2b24a317a6bf334aad7897fe8ff1a8b2f97c69ad
Wesley Miranda <wesleywes42@gmail.com>
2022-10-19 23:03:37 -0600

    added test for genre

--- spec/requests/api/v1/rawg_search_request_spec.rb
+++ spec/requests/api/v1/rawg_search_request_spec.rb
@@ -31,4 +31,33 @@ RSpec.describe 'Games API' do
       expect(game[:game_id]).to be_a Integer
     end
   end
+  it 'sends a list of games with genres included', :vcr do
+    info = {
+      "search": "dog",
+      "genres": "shooter"
+    }
+
+    get api_v1_search_index_path(info)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    games = JSON.parse(response.body, symbolize_names: true)
+    expect(games.count).to eq(20)
+    expect(games).to be_a(Array)
+    expect(games).to be_all(Hash)
+
+    games.each do |game|
+      expect(game).to have_key(:name)
+      expect(game[:name]).to be_a String
+      expect(game).to have_key(:platforms)
+      expect(game[:platforms]).to be_a Array
+      expect(game).to have_key(:image)
+      expect(game[:image]).to be_a String
+      expect(game).to have_key(:genres)
+      expect(game[:genres]).to be_a Array
+      expect(game).to have_key(:game_id)
+      expect(game[:game_id]).to be_a Integer
+    end
+  end
 end
