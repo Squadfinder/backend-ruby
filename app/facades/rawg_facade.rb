e2e9132b62696827cc191108e28ca6cd0e6082a3
Blake Saylor <Blakesaylor@gmail.com>
2022-10-22 14:46:53 -0600

    Add sad path testing for Game search; Update fixtures

--- app/facades/rawg_facade.rb
+++ app/facades/rawg_facade.rb
@@ -2,6 +2,10 @@ class RawgFacade
 
   def self.get_game(game_id)
     data = RawgSearchService.search_game_id(game_id)
+    if data.keys.include?(:detail)
+      nil
+    else
       Game.new(data)
+    end
   end
 end
